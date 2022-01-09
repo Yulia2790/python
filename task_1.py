6af7ba289c754d3f4cb9fb53a38f19531e61e039
Yulia <Juljok_27@mail.ru>
2022-01-09 23:42:34 +0300

    Task_1

---
+++ task_1.py
@@ -0,0 +1,9 @@
+
+list = [27, False, 'and', 15.55, [7, 9]]
+print(list)
+print(type(list[0]))
+print(type(list[1]))
+print(type(list[2]))
+print(type(list[3]))
+print(type(list[4]))
+
---
+++ task_4.py
@@ -0,0 +1,10 @@
+
+s1 = 'At home I have a cat Phoebe'
+print(s1.split())
+for i, element in enumerate(s1.split(), 1):
+    print(i, element)
+
+
+
+
+
